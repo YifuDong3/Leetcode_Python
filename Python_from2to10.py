25a05ceeb98dbb0234904e5bd2add7b40aff8926
Yifu Dong <a455650509@qq.com>
2018-10-26 13:12:11 -0400

    Updated version_Oct26

--- Python_from2to10.py
+++ Python_from2to10.py
@@ -78,7 +78,78 @@ a4.findMedianSortedArray(num1,num2)
 
 ######################################################################
 #leetcode: problem5---- Longest Palindromic Substring.
+class Solution5(object):
+	def longestPalindrome(self, s):
+
+		if len(s)==0 or len(s)==1:
+			return s
+		sub=s[0:1]
+		for i in range(len(s)-1):
+			def ishuiwen(s,x,y):
+				while x>=0 and y<len(s) and s[x]==s[y]:
+					x-=1
+					y+=1
+				return s[x+1:y]
+			s1=ishuiwen(s,i,i)
+			if len(s1)>len(sub):
+				sub=s1
+			s2=ishuiwen(s,i,i+1)
+			if len(s2)>len(sub):
+			sub=s2
+		return sub
+a=Solution5()
+a.longestPalindrome("dfasdfc")
 
 
 
+############################################
+#problem 6 -----Zig Zag conversion
+
+class Solution6:
+	def convert(self, s, numRows):
+		"""
+		:type s: str
+		:type numRows: int
+		:rtype: str
+		"""
+		if numRows == 1 or numRows >= len(s):
+			return s
+
+		L = [''] * numRows
+		index, step = 0, 1
+
+		for x in s:
+			L[index] += x
+			if index == 0:
+				step = 1
+			elif index == numRows - 1:
+				step = -1
+			index += step
+
+		return ''.join(L)
+
+
+#######################################################
+## problem 7 : reverse 32-bit problem
+class Solution7:
+	def reverse(self, x):
+		n = abs(x)
+		s = str(n)
+		a = []
+
+		m = ''
+		for i in range(len(s)):
+			a.append(s[i])
+		la=len(a)
+		if x < 0:
+			m += '-'
+
+		while la>0:
+			m += a.pop()
+		la -= 1
+		x = int(m)
+		if abs(x) > 2**31:
+			return 0
+		else:
+			return x
 
