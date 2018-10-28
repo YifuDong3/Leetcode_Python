2e019770647ff309cb38de1ab494ef1487d3a3a6
Yifu Dong <a455650509@qq.com>
2018-10-28 17:20:40 -0400

    Algorithms without data structure are like shit
    Some tips about how to use dictionary. Additionally, time to learn data structure.

---
+++ python_11to15.py
@@ -0,0 +1,62 @@
+class Solution11:
+	def maxArea(self, height):
+		l, r = 0, len(height) - 1
+		max_area = 0
+		while l < r:
+			curr_area = min(height[l], height[r]) * (r - l)
+			max_area = max(curr_area, max_area)
+
+			if height[l] > height[r]:
+				r -= 1
+			else:
+				l += 1
+
+		return max_area
+
+
+
+########################################################################
+#solution 12:
+
+class Solution12:
+	r = {
+        # value is an array of strings for each key in
+        # the ones, tens, hundreds, and thousands places
+        # up to 3000 (since given max is 3999)
+        1: ['I', 'X', 'C', 'M'],
+        2: ['II', 'XX', 'CC', 'MM'],
+        3: ['III', 'XXX', 'CCC', 'MMM'],
+        4: ['IV', 'XL', 'CD'],
+        5: ['V', 'L', 'D'],
+        6: ['VI', 'LX', 'DC'],
+        7: ['VII', 'LXX', 'DCC'],
+        8: ['VIII', 'LXXX', 'DCCC'],
+        9: ['IX', 'XC', 'CM']
+    }
+
+	def intToRoman(self, num):
+		"""
+        :type num: int
+        :rtype: str
+        """
+		ret = ""
+		for i in range(len(str(num))):
+			digit = (num // (10 ** i)) % 10
+			if digit > 0:
+				ret = self.r[digit][i] + ret
+		return ret
+
+################################################################################
+#solution13:
+
+class Solution13:
+	def romanToInt(self, s):
+		num = 0
+		dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': -2, 'IX': -2, 'XL': -20,
+				'XC': -20, 'CD': -200, 'CM': -200}
+		for key in dict:
+			num = num + s.count(key) * dict[key]
+		return num
+
+
+
