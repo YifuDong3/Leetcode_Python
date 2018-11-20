0429cb064c6e235b5233dde09e38a41438f68514
Yifu Dong <a455650509@qq.com>
2018-11-20 02:00:06 -0500

    loop from 2 sides of a data structure
    loop from 2 sides of a data structure; usage of map

--- python_16to20.py
+++ python_16to20.py
@@ -19,3 +19,27 @@ class Solution:
 					k -= 1
 
 		return result
+
+
+#17:Letter Combinations of a Phone Number
+class Solution:
+	def letterCombinations(self, digits):
+		"""
+		:type digits: str
+		:rtype: List[str]
+		"""
+
+		if len(digits) == 0:
+			return []
+		d = {'2': 'abc',
+			 '3': 'def',
+			 '4': 'ghi',
+			 '5': 'jkl',
+			 '6': 'mno',
+			 '7': 'pqrs',
+			 '8': 'tuv',
+			 '9': 'wxyz'}
+		acc = [e for e in d[digits[0]]]  # Avoiding a always empty list in for loop
+		for digit in digits[1:]:
+			acc = [w1 + w2 for w1 in acc for w2 in d[digit]]
+		return acc
